6f1c4bc2e32eb1c5a63c1cc13d3b00e42291579f
marshalllussier <marshallelussier@gmail.com>
2022-03-07 00:00:51 -0600

    Adds camera button that displays an image you take

--- app/src/main/java/com/example/facialrecognition/MainActivity.java
+++ app/src/main/java/com/example/facialrecognition/MainActivity.java
@@ -6,16 +6,24 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.hardware.Camera;
+import android.hardware.camera2.CameraDevice;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button storage, camera;
-    private static final int CAMERA_PERMISSION_CODE = 100;
-    private static final int STORAGE_PERMISSION_CODE = 101;
+    private Button camera_open_id;
+    private ImageView image_id;
+    private static final int pic_id = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -23,56 +31,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        storage = findViewById(R.id.storage);
-        camera = findViewById(R.id.camera);
+        camera_open_id = (Button)findViewById(R.id.camera_button);
+        image_id = (ImageView)findViewById(R.id.click_image);
+        camera_open_id.setOnClickListener(v -> {
 
-        // Set Buttons on Click Listeners
-        storage.setOnClickListener(v -> checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, STORAGE_PERMISSION_CODE));
-
-        camera.setOnClickListener(v -> checkPermission(Manifest.permission.CAMERA, CAMERA_PERMISSION_CODE));
+            Intent camera_intent
+                    = new Intent(MediaStore
+                    .ACTION_IMAGE_CAPTURE);
+            startActivityForResult(camera_intent, pic_id);
+        });
     }
 
-    // Function to check and request permission.
-    public void checkPermission(String permission, int requestCode)
-    {
-        if (ContextCompat.checkSelfPermission(MainActivity.this, permission) == PackageManager.PERMISSION_DENIED) {
+    protected void onActivityResult(int requestCode,
+                                    int resultCode,
+                                    Intent data) {
 
-            // Requesting the permission
-            ActivityCompat.requestPermissions(MainActivity.this, new String[] { permission }, requestCode);
-        }
-        else {
-            Toast.makeText(MainActivity.this, "Permission already granted", Toast.LENGTH_SHORT).show();
-        }
-    }
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == pic_id) {
 
-    // This function is called when the user accepts or decline the permission.
-    // Request Code is used to check which permission called this function.
-    // This request code is provided when the user is prompt for permission.
+            Bitmap photo = (Bitmap) data.getExtras()
+                    .get("data");
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String[] permissions,
-                                           @NonNull int[] grantResults)
-    {
-        super.onRequestPermissionsResult(requestCode,
-                permissions,
-                grantResults);
 
-        if (requestCode == CAMERA_PERMISSION_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(MainActivity.this, "Camera Permission Granted", Toast.LENGTH_SHORT) .show();
-            }
-            else {
-                Toast.makeText(MainActivity.this, "Camera Permission Denied", Toast.LENGTH_SHORT) .show();
-            }
-        }
-        else if (requestCode == STORAGE_PERMISSION_CODE) {
-            if (grantResults.length > 0
-                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(MainActivity.this, "Storage Permission Granted", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(MainActivity.this, "Storage Permission Denied", Toast.LENGTH_SHORT).show();
-            }
+            image_id.setImageBitmap(photo);
         }
     }
 }
