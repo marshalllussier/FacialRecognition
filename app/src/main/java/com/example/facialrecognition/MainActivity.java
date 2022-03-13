9e888e3c4bfcab974e2f996fef2d8b5b8dc3dd1a
marshalllussier <marshallelussier@gmail.com>
2022-03-13 14:36:53 -0500

    Adds capture button that allows you to take a picture that will be saved to your camera roll.

--- app/src/main/java/com/example/facialrecognition/MainActivity.java
+++ app/src/main/java/com/example/facialrecognition/MainActivity.java
@@ -1,59 +1,153 @@
 package com.example.facialrecognition;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.content.Intent;
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.hardware.Camera;
-import android.hardware.camera2.CameraDevice;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
 
-    private Button camera_open_id;
-    private ImageView image_id;
-    private static final int pic_id = 123;
+import com.google.common.util.concurrent.ListenableFuture;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+
+public class MainActivity extends AppCompatActivity implements ImageAnalysis.Analyzer, View.OnClickListener {
+    PreviewView previewView;
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    private ImageCapture imageCapture;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        camera_open_id = (Button)findViewById(R.id.camera_button);
-        image_id = (ImageView)findViewById(R.id.click_image);
-        camera_open_id.setOnClickListener(v -> {
-
-            Intent camera_intent
-                    = new Intent(MediaStore
-                    .ACTION_IMAGE_CAPTURE);
-            startActivityForResult(camera_intent, pic_id);
-        });
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            openCamera();
+            return;
+        }
+        requestPermissions(new String[] { Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 104);
     }
 
-    protected void onActivityResult(int requestCode,
-                                    int resultCode,
-                                    Intent data) {
+    public void openCamera() {
+        previewView = findViewById(R.id.previewView);
+        Button bCapture = findViewById(R.id.bCapture);
+//        bCapture.setText("CAPTURE PHOTO");        the bCapture Button can be manipulated manually through code as well as in the res layout
 
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == pic_id) {
+        bCapture.setOnClickListener(this);
 
-            Bitmap photo = (Bitmap) data.getExtras()
-                    .get("data");
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                startCameraX(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, getExecutor());
+    }
 
-
-            image_id.setImageBitmap(photo);
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 104) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openCamera();
+            } else {
+                Toast.makeText(MainActivity.this, "Insufficient Permissions", Toast.LENGTH_LONG).show();
+            }
         }
+    }
+
+    Executor getExecutor() {
+        return ContextCompat.getMainExecutor(this);
+    }
+
+    @SuppressLint("RestrictedApi")
+    private void startCameraX(ProcessCameraProvider cameraProvider) {
+        cameraProvider.unbindAll();
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+//                .requireLensFacing(CameraSelector.LENS_FACING_FRONT)          This should be uncommented and the above line should be commented for our app, but for testing purposes this is easier to work with.
+                .build();
+        Preview preview = new Preview.Builder()
+                .build();
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+        imageCapture = new ImageCapture.Builder()
+                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                .build();
+        ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build();
+        imageAnalysis.setAnalyzer(getExecutor(), this);
+        cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview, imageCapture);
+    }
+
+    @Override
+    public void analyze(@NonNull ImageProxy image) {
+        Log.d("TAG", "Analyzer: Frame found at: " + image.getImageInfo().getTimestamp());
+        image.close();
+    }
+
+    @SuppressLint({"RestrictedApi", "NonConstantResourceId"})
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.bCapture:
+                capturePhoto();
+                break;
+        }
+    }
+
+
+    private void capturePhoto() {
+        long timestamp = System.currentTimeMillis();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, timestamp);
+        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
+
+
+        imageCapture.takePicture(
+                new ImageCapture.OutputFileOptions.Builder(
+                        getContentResolver(),
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        contentValues
+                ).build(),
+                getExecutor(),
+                new ImageCapture.OnImageSavedCallback() {
+                    @Override
+                    public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                        Toast.makeText(MainActivity.this, "Photo has been saved.", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(@NonNull ImageCaptureException exception) {
+                        Toast.makeText(MainActivity.this, "ERROR: " + exception.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+
     }
 }
