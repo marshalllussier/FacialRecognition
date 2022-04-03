9841a94eb532eb610a61322446817c59bec346e8
marshalllussier <marshallelussier@gmail.com>
2022-04-03 16:00:09 -0500

    Adds development functionality to switch between facial detection showing boundary box or contour points

--- app/src/main/java/com/autozone/facialrecognition/ShowContourPoint.java
+++ app/src/main/java/com/autozone/facialrecognition/ShowContourPoint.java
@@ -3,31 +3,30 @@ package com.autozone.facialrecognition;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.PointF;
 import android.graphics.Rect;
 import android.view.View;
 
-public class ShowFaceBox extends View {
+public class ShowContourPoint extends View {
 
-    private Rect rectangle_box;
+    private PointF pointF;
     private Paint paint;
 
-    public ShowFaceBox(Context context, Rect facebox) {
+    public ShowContourPoint(Context context, PointF point) {
         super(context);
 
-        rectangle_box = facebox;
+        pointF = point;
         paint = new Paint();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        paint.setColor(getResources().getColor(R.color.autozoneSecondary));
+        paint.setColor(getResources().getColor(R.color.white));
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeWidth(4);
-        rectangle_box.bottom*=1.5;
-        rectangle_box.top*=1.5;
-        rectangle_box.left*=1.5;
-        rectangle_box.right*=1.5;
-        canvas.drawRect(rectangle_box, paint);
+        pointF.x *=1.6;
+        pointF.y *=1.6;
+        canvas.drawPoint(pointF.x, pointF.y, paint);
         this.setVisibility(View.GONE);
     }
 }
