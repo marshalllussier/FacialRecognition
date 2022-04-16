ef77f5fb5e3ed47a78f31a9e86c34b097714e52f
marshalllussier <marshallelussier@gmail.com>
2022-04-16 02:02:11 -0500

    - Gradle update - Fixes FileNotFound exception handling

--- app/src/main/java/com/autozone/facialrecognition/Logon.java
+++ app/src/main/java/com/autozone/facialrecognition/Logon.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -55,7 +56,7 @@ public class Logon {
             return line.split(":", 2)[1];
         } catch (Exception exc) {
             Log.e("Exception", "Unable to fetch last logon. Error: " + exc);
-            return exc.toString();
+            return "";
         }
     }
 }
